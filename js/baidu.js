8de9482ba8e6d410eedb2dad79501ec7b04cb480
lewiscutey <949124363@qq.com>
2016-12-01 23:42:11 +0800

    'a'

--- js/baidu.js
+++ js/baidu.js
@@ -2,6 +2,12 @@
  * Created by Lewis on 2016/11/29.
  */
 $(function(){
+    // document.on("mousedown",function (e) {
+    //     e.preventDefault();
+    // });
+    // document.on("mousemove",function (e) {
+    //     e.preventDefault();
+    // });
     var clientH=$(window).height();
     var num=0;
     var flag=true;
@@ -44,12 +50,45 @@ $(function(){
 });
 
 $(function(){
+    var arrow=$(".arrow");
+    arrow.click(function(){
+        $.fn.fullpage.moveSectionDown();
+    });
     $('#fullpage').fullpage({
         'verticalCentered': false,
         'css3': true,
         anchors: ['page1', 'page2', 'page3', 'page4'],
-        menu:true,
         'navigationPosition': 'right',
-        'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple']
-    })
+        navigation: true,
+        easing:"linear",
+        navigationTooltips:['登录百度舆情','领先：数据收集与处理','全面：舆情分析逻辑与架构','专业：专业数据可视化'],
+
+        afterLoad: function(anchorLink, index){
+            if(index==1){
+                $('.section').eq(0).attr("id","active");
+            }else{
+                $('.section').eq(0).removeAttr("id");
+            }
+            if(index==2){
+                $('.section').eq(1).attr("id","active");
+            }else{
+                $('.section').eq(1).removeAttr("id");
+            }
+            if(index==3){
+                $('.section').eq(2).attr("id","active");
+            }else{
+                $('.section').eq(2).removeAttr("id");
+            }
+            if(index==4){
+                $('.section').eq(3).attr("id","active");
+            }else{
+                $('.section').eq(3).removeAttr("id");
+            }
+        }
+
+
+
+
+
+    });
 });
